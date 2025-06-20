903158e692b05f0908765fd7ed9373ff9da431fc
KushBhakkad <kushbhakkad@gmail.com>
2025-06-20 16:03:29 +0530

    Resolved error: '401 Unauthorized on GET request' by adding Http headers and cookies

--- src/main/java/com/kushbhakkad/nseapicall/dto/AnnouncementDto.java
+++ src/main/java/com/kushbhakkad/nseapicall/dto/AnnouncementDto.java
@@ -1,7 +1,9 @@
 package com.kushbhakkad.nseapicall.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AnnouncementDto {
 
     @JsonProperty("sm_name")
--- src/main/java/com/kushbhakkad/nseapicall/service/NseApiService.java
+++ src/main/java/com/kushbhakkad/nseapicall/service/NseApiService.java
@@ -4,9 +4,9 @@ import com.kushbhakkad.nseapicall.dto.AnnouncementDto;
 import com.kushbhakkad.nseapicall.model.Announcement;
 import com.kushbhakkad.nseapicall.repository.AnnouncementRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -19,20 +19,49 @@ public class NseApiService {
     private AnnouncementRepository repository;
 
     private static final String NSE_URL = "https://www.nseindia.com/api/corporate-announcements?index=equities";
+    private static final String NSE_HOME = "https://www.nseindia.com/";
 
     public void fetchAndSaveAnnouncements() {
-        RestTemplate restTemplate = new RestTemplate();
-        String jsonResponse = restTemplate.getForObject(NSE_URL, String.class);
-
         try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            List<AnnouncementDto> dtoList = objectMapper.readValue(jsonResponse, new TypeReference<List<AnnouncementDto>>() {});
-            for (AnnouncementDto dto : dtoList) {
-                Announcement a = new Announcement();
-                a.setSmName(dto.getSmName());
-                a.setAttchmntText(dto.getAttchmntText());
-                repository.save(a);
+            RestTemplate restTemplate = new RestTemplate();
+
+            // Step 1: Hit NSE homepage to retrieve cookies
+            HttpHeaders homeHeaders = new HttpHeaders();
+            homeHeaders.set("User-Agent", "Mozilla/5.0");
+            homeHeaders.set("Accept", "text/html,application/xhtml+xml,application/xml");
+            homeHeaders.set("Accept-Language", "en-US,en;q=0.9");
+
+            HttpEntity<String> homeRequest = new HttpEntity<>(homeHeaders);
+            ResponseEntity<String> homeResponse = restTemplate.exchange(NSE_HOME, HttpMethod.GET, homeRequest, String.class);
+
+            List<String> cookies = homeResponse.getHeaders().get("Set-Cookie");
+            String cookieHeader = String.join("; ", cookies);
+
+            // Step 2: Use cookies to hit the API
+            HttpHeaders apiHeaders = new HttpHeaders();
+            apiHeaders.set("User-Agent", "Mozilla/5.0");
+            apiHeaders.set("Accept", "application/json");
+            apiHeaders.set("Referer", "https://www.nseindia.com/");
+            apiHeaders.set("Cookie", cookieHeader);
+
+            HttpEntity<String> apiRequest = new HttpEntity<>(apiHeaders);
+            ResponseEntity<String> response = restTemplate.exchange(NSE_URL, HttpMethod.GET, apiRequest, String.class);
+
+            if (response.getStatusCode() == HttpStatus.OK) {
+                String jsonResponse = response.getBody();
+                ObjectMapper mapper = new ObjectMapper();
+                List<AnnouncementDto> dtoList = mapper.readValue(jsonResponse, new TypeReference<List<AnnouncementDto>>() {});
+                for (AnnouncementDto dto : dtoList) {
+                    Announcement announcement = new Announcement();
+                    announcement.setSmName(dto.getSmName());
+                    announcement.setAttchmntText(dto.getAttchmntText());
+                    repository.save(announcement);
+                    System.out.println("Saving: " + dto.getSmName() + " | " + dto.getAttchmntText());
+                }
+            } else {
+                System.out.println("Failed to fetch data from NSE. Status code: " + response.getStatusCode());
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
